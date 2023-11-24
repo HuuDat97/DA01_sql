aa33164e239ef133935538089bb2631c3335696d
HuuDat97 <150250662+HuuDat97@users.noreply.github.com>
2023-11-24 15:59:31 +0900

    day13_baitap.sql

--- day13_baitap.sql
+++ day13_baitap.sql
@@ -29,19 +29,63 @@ FROM b
 where ranking <=2
 
 ---ex3
-
+WITH call AS
+(
+SELECT  policy_holder_id,
+COUNT(case_id) AS so_cuoc_goi
+FROM callers
+GROUP BY policy_holder_id
+)
+SELECT
+COUNT(*) AS member_count
+FROM call
+WHERE so_cuoc_goi >= 3
 
 ---ex4
-
+SELECT pages.page_id FROM pages
+FULL OUTER JOIN page_likes ON pages.page_id = page_likes.page_id
+WHERE liked_date is NULL
+ORDER BY pages.page_id ASC  ---đã có lời giải sẵn trong link
 
 ---ex5
-
+WITH cte AS 
+(
+  SELECT user_id,
+    EXTRACT(MONTH FROM event_date) AS mth
+  FROM user_actions
+  WHERE event_type IN ('sign-in','like','comment')
+    AND EXTRACT (MONTH FROM event_date) IN (6,7)
+    AND date_part ('year', event_date) = 2022
+  GROUP BY user_id, mth
+  HAVING COUNT(EXTRACT(MONTH FROM event_date))  = 2
+)
+SELECT mth, 
+COUNT(user_id) AS active_user
+FROM cte
+WHERE mth = 7
+GROUP BY mth
 
 ---ex6
-
+SELECT
+    DATE_FORMAT(trans_date, '%Y-%m') AS month,
+    country,
+    COUNT(id) AS trans_count,
+    COUNT(CASE WHEN state = 'approved' THEN id END) AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(CASE WHEN state = 'approved' THEN amount END) AS approved_total_amount
+FROM
+    transactions
+GROUP BY
+    month, country
 
 ---ex7
-
+select 
+distinct(product_id),
+year as first_year,
+quantity,
+price 
+from Sales 
+group by product_id
 
 ---ex8
 
